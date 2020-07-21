ac8e5cc0c1d06590ca274c8b0e4c57deca385c90
domancheta <domancheta@gmail.com>
2020-07-20 21:25:07 -0700

    http client experimentations to ebay dev site

--- Ebaytools/src/main/org/allthegoodstuff/ebaytools/ShopHttpClient.java
+++ Ebaytools/src/main/org/allthegoodstuff/ebaytools/ShopHttpClient.java
@@ -5,14 +5,34 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URI;
 
+/*Fields from the item are retrieved on the perl app:
+        itemID
+        EndTime
+        itemTitle
+        image URLs ( There's a gallery url and list of picture urls being returned in api)
+        item description - appears in api with extra params
+        shipping info (multiple fields)
+        -
+        bid amount (one of) - in model, this is currentPrice:
+        - current bid
+        - winning bid
+        - start price
+        bid count
+        highest bidder (one of):
+        - high bidder
+        - winning bidder
+        - seller
+*/
+
 public class ShopHttpClient {
 
-    // TODO: synchronous get call - use asynchronous instead
+    // TODO: synchronous get call - use asynchronous instead?
     public static String getSingleItem(String itemID) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
         String uri = "https://open.api.ebay.com/shopping?callname=GetSingleItem&" +
-               "responseencoding=JSON&appid=DominicA-eShopToo-PRD-8c8ee5576-77674917&" +
-               "siteid=0&version=967&ItemID=" + itemID;
+               "responseencoding=XML&appid=DominicA-eShopToo-PRD-8c8ee5576-77674917&" +
+               "siteid=0&version=967&ItemID=" + itemID +
+               "&IncludeSelector=Description,Details";
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .build();
--- Ebaytools/src/test/org/allthegoodstuff/ebaytools/ShopHttpClientTest.java
+++ Ebaytools/src/test/org/allthegoodstuff/ebaytools/ShopHttpClientTest.java
@@ -6,8 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShopHttpClientTest {
 
     @Test
-    void getSingleItemTest() {
+    void getSingleAuctionItemTest() {
         String itemID = "402323900275";
+        String rs;
+
+        try {
+            rs = ShopHttpClient.getSingleItem(itemID);
+
+            System.out.println(rs);
+        }
+        catch (Exception e) {
+            System.err.println("Exception thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void getSingleAuctionItemBidsTest() {
+        String itemID = "264787227715";
         String rs;
 
         try {
