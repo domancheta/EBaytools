03c7167b3852f7a90f481f5d7b91a38fd586f00a
domancheta <domancheta@gmail.com>
2020-07-24 14:07:51 -0700

    Set up Search Textfield to do the http call

--- src/main/org/allthegoodstuff/ebaytools/view/RootLayoutController.java
+++ src/main/org/allthegoodstuff/ebaytools/view/RootLayoutController.java
@@ -2,9 +2,30 @@ package org.allthegoodstuff.ebaytools.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import org.allthegoodstuff.ebaytools.EBayToolsMain;
+import org.allthegoodstuff.ebaytools.ShopHttpClient;
 
 public class RootLayoutController {
+
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    private void initialize() {
+        searchText.setOnAction((event) -> {
+            try {
+                String rawEbayResponse = ShopHttpClient.getSingleItem(searchText.getText());
+                System.out.println(rawEbayResponse);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            searchText.clear();
+        });
+    }
+
     /**
      * Is called by the main application to give a reference back to itself.
      */
