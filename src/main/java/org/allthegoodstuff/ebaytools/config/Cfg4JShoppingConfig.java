a245e12203fbe140a75e933b53de6d20c2c41cbf
domancheta <domancheta@gmail.com>
2020-08-01 04:34:47 -0700

    add config properties file and logic to retrieve from it

---
+++ src/main/java/org/allthegoodstuff/ebaytools/config/Cfg4JShoppingConfig.java
@@ -0,0 +1,70 @@
+package org.allthegoodstuff.ebaytools.config;
+
+import org.cfg4j.provider.ConfigurationProvider;
+import org.cfg4j.provider.ConfigurationProviderBuilder;
+import org.cfg4j.source.ConfigurationSource;
+import org.cfg4j.source.context.environment.Environment;
+import org.cfg4j.source.context.environment.ImmutableEnvironment;
+import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
+import org.cfg4j.source.files.FilesConfigurationSource;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Cfg4JShoppingConfig implements ShoppingConfig {
+
+    private static ConfigurationProvider configProvider;
+
+    static {
+        // TODO: probably move config file somewhere in class structure and use the classpath provider instead
+        ConfigFilesProvider configFilesProvider = () -> Arrays.asList(Paths.get("config.yaml"));
+        ConfigurationSource configSource = new FilesConfigurationSource(configFilesProvider);
+        Environment env = new ImmutableEnvironment(".");
+
+        configProvider = new ConfigurationProviderBuilder().withConfigurationSource(configSource)
+                .withEnvironment(env)
+                .build();
+    }
+
+    public Cfg4JShoppingConfig() {
+    }
+
+    public String baseApiUri() {
+       return configProvider.getProperty("baseApiUri", String.class);
+    }
+
+    public String apiVersion() {
+        return configProvider.getProperty("apiVersion", String.class);
+    }
+
+    public String appID() {
+        return configProvider.getProperty("appid", String.class);
+    }
+
+    public String callname() {
+        return configProvider.getProperty("getItemCallname", String.class);
+    }
+
+    public String itemStatusCallname() {
+        return configProvider.getProperty("getItemStatusCallname", String.class);
+    }
+
+    public String rsEncoding() {
+        return configProvider.getProperty("rsEncoding", String.class);
+    }
+
+    public String siteid() {
+        return configProvider.getProperty("siteid", String.class);
+    }
+
+    public String includeSelector(){
+        return configProvider.getProperty("includeDetailsSelector", String.class);
+    }
+
+    // generic get to retrieve any custom property in config file(s)
+    public String getProperty(String key) {
+        return configProvider.getProperty(key, String.class);
+    }
+
+
+}
---
+++ src/main/java/org/allthegoodstuff/ebaytools/config/ShoppingConfig.java
@@ -0,0 +1,26 @@
+package org.allthegoodstuff.ebaytools.config;
+
+public interface ShoppingConfig {
+
+    // property strings to be used to build out fetch API URIs
+    public String baseApiUri();
+
+    public String apiVersion();
+
+    public String appID();
+
+    // call to retrieve info on single item
+    public String callname();
+
+    // call to retrieve status info on one or more item - use for quick retrieval of bidding info
+    public String itemStatusCallname();
+
+    public String rsEncoding();
+
+    public String siteid();
+
+    public String includeSelector();
+
+    // generic method to retrieve specified property from the config files
+    public String getProperty(String key);
+}
