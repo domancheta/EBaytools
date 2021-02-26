d27d13b3bb92c726879e8d7fe2256ebcdaf4653e
domancheta <domancheta@gmail.com>
2021-02-25 20:57:55 -0800

    a few tweaks to item status class before refactor

--- src/main/java/org/allthegoodstuff/ebaytools/FetchItemStatusResult.java
+++ src/main/java/org/allthegoodstuff/ebaytools/FetchItemStatusResult.java
@@ -1,2 +1,41 @@
-package org.allthegoodstuff.ebaytools;public class FetchItemStatusResult {
+package org.allthegoodstuff.ebaytools;
+
+import org.allthegoodstuff.ebaytools.model.SaleItem;
+import org.eclipse.jgit.annotations.Nullable;
+
+public final class FetchItemStatusResult {
+    protected enum Status {FAILED, SUCCEEDED} ;
+
+    private Status status;
+    private String errorMessage;
+    private SaleItem saleItem;
+
+    //todo: get the info fields in ebay GetItemStatus response that's needed
+    // tentatively these would be Errors/ShortMessage, BidCount, ListingStatus, EndTime
+    // todo: new columns and/or tables in database to add
+    //todo: make a Result interface and use that as base for the 2 result types
+    //todo: make this generic so it can be non-specific to ebay
+    public FetchItemStatusResult(Status status, @Nullable String errorMessage, @Nullable SaleItem saleItem) {
+        this.status = status;
+        if (status == Status.SUCCEEDED) {
+            this.saleItem = saleItem;
+        } else {
+            this.errorMessage = errorMessage;
+        }
+    }
+
+    public boolean fetchSucceeded() {
+        if (status == Status.SUCCEEDED)
+            return true;
+        return false;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public SaleItem getSaleItem() {
+        return saleItem;
+    }
 }
+
